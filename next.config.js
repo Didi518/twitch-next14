51ff41d0998b2d61679b7045a73a23c0316627b4
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-12-28 05:04:03 +0100

    :closed_lock_with_key: uploadthing/miniatures et infos stream

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const { config } = require("process");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["utfs.io"],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.mjs$/,
